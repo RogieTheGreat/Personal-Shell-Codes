ede32de51cf2f772372191228ec44f651fa69a59
RogieTheGreat <rmbernabe.de@gmail.com>
2026-07-21 11:22:20 +1000

    navigation.sh - basic cmd

---
+++ navigation.sh
@@ -0,0 +1,76 @@
+echo Hi! My name is ...
+
+pwd 
+
+# Path                                                  
+# ----                                                  
+# C:\Users\bernabe\Documents\GitHub\Personal-Shell-Codes
+
+# cd directory-name
+
+ls
+
+
+#     Directory: C:\Users\bernabe\Documents\GitHub\Personal-Shell-Codes
+
+
+# Mode                 LastWriteTime         Length Name                                                                                                                         
+# ----                 -------------         ------ ----                                                                                                                         
+# -a----        21/07/2026  11:08 AM            224 navigation.sh                                                                                                                
+# -a----        21/07/2026  10:58 AM            140 notes.txt                                                                                                                    
+# -a----        21/07/2026  10:56 AM            271 Port Sniffer.sh                                                                                                              
+# -a----        21/07/2026  10:56 AM           6188 PowerShell_Cheat_Sheet.md    
+
+
+ls C:\Users\bernabe\Downloads
+
+
+#     Directory: C:\Users\bernabe\Downloads
+
+
+# Mode                 LastWriteTime         Length Name                                                                                                                         
+# ----                 -------------         ------ ----                                                                                                                         
+# d-----        21/07/2026  11:09 AM                house     
+
+
+pwd
+
+# Current Location
+
+ls
+
+echo "Checking parent folder"
+
+ls ..
+
+echo "Checking GitHub folder"
+
+ls ../
+
+echo "Checking Documents folder"
+
+ls ../../
+
+echo "Done exploring"
+
+
+# Navigating the folder
+
+ls C:\Users\bernabe\Downloads\house\house\bathroom\closet
+
+#     Directory: C:\Users\bernabe\Downloads\house\house\bathroom\closet
+
+
+# Mode                 LastWriteTime         Length Name                                                                                                                         
+# ----                 -------------         ------ ----                                                                                                                         
+# ------         9/11/2023  12:01 PM             44 items.txt          
+
+# Viewing File Content
+
+cat C:\Users\bernabe\Downloads\house\house\bathroom\closet\items.txt
+
+# ðŸª¥ Toothbrush
+# ðŸ§¼ Soap
+# ðŸ§» Toilet Paper
+
+clear
