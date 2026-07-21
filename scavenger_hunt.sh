f2a6279bf534bfd765e53a90a8a16e6e47bd3ff4
RogieTheGreat <rmbernabe.de@gmail.com>
2026-07-21 11:27:55 +1000

    learning how to scavenger

---
+++ scavenger_hunt.sh
@@ -0,0 +1,31 @@
+: '
+As a refresher, here is a breakdown of the commands you should use:
+
+Command	Description
+echo output-here	Print what is typed next to this command
+pwd	Print the current working directory
+cat file-name.extension	Print the contents of a file
+ls	List the contents of the current directory
+cd directory-name	Change into an existing directory
+cd ..	Change backward into an existing directory
+cd directory-name/sub-directory-name	Change forward on multiple directories
+cd ../..	Change backward on multiple directories
+clear	Clear command line
+'
+
+
+ls C:\Users\bernabe\Downloads
+
+ls C:\Users\bernabe\Downloads\scavenger-hunt\scavenger-hunt
+
+: '
+cat C:\Users\bernabe\Downloads\scavenger-hunt\scavenger-hunt\welcome.txt
+Hello scavenger! ðŸ‘‹
+
+Welcome to the Room of Mirrors. It's time to put it all to the test.
+
+Go forth with courage and may the spirit of Command Line be at your side.
+
+ðŸªž ðŸªž ðŸªž ðŸªž ðŸšª ðŸªž
+'
+
