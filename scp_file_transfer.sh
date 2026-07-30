349429f4b990b742e37dba6441975eafd55f1c1c
Ji <rmbernabe.de@gmail.com>
2026-07-30 10:32:32 +1000

    Create scp_file_transfer.sh with usage comments
    Add usage examples for SCP file transfer script

---
+++ scp_file_transfer.sh
@@ -0,0 +1,9 @@
+# scp source destination
+
+# Uses wildcard as destination
+# scp pi@rogie-lab.local:/home/pi/file.pdf .
+# scp pi@rogie-lab.local:/home/pi/*.pdf .
+
+# If I want multiple or folder
+
+# scp -r pi@rogie-lab.local:/home/pi/Bookshelf .
